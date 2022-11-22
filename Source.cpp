23c4d97c45713d54c9600e4995bb6c1ce67024ca
Lumine11 <kt061103@gmail.com>
2022-11-22 09:54:38 +0700

    cmd 4 5

--- Source.cpp
+++ Source.cpp
@@ -10,58 +10,86 @@ using namespace std;
 #include"GenerateData.h" // header chứa Generate Data
 #include"Timing.h" // header chứ các thuật toán sắp xếp _ đo thời gian
 #include "CountingComparision.h"// header chứa các thuật toán sắp xếp - đo số lần so sánh
+#include "cmd.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    int size[6] = { 10000, 30000, 50000, 100000, 300000, 500000 };
-    vector <string> Name = { "SelectionSort", "Insertion_Sort", "Bubble_sort", "Shaker_Sort", "Shell_Sort", "Heap_Sort" , "Merge_Sort" , "Quick_Sort" , "Counting_Sort", "Radix_Sort", "Flash_Sort" };
-    vector <string> Order = { "RandomData" , "SortedData", "ReverseData", "NearlySortedData" };
-    clock_t start, end;   // Khai báo biến thời gian
-
-    ofstream fout("output.txt");
-
-    for (int OrderNo = 0; OrderNo < 4; OrderNo++) {
-        fout << "Data order:" << Order[OrderNo] << endl;
-        cout << "Kind of order: " << Order[OrderNo] << endl;
-        for (int id = 0; id < 6; id++) // size
-        {
-            int n = size[id];
-            int* a = new int[n];
-            GenerateData(a, size[id], OrderNo);
-            fout << "Data size" << n << endl;
-            cout << "   *size = " << n << endl;
-
-
-            for (int sort = 0; sort < 11; sort++)
-            {
-                int* b = new int[n];
-                int* c = new int[n];
-
-                for (int i = 0; i < size[id]; i++)
-                    c[i] = b[i] = a[i];
-
-                fout << "       Kind of sort: " << Name[sort] << endl;
-                cout << "       Kind of sort: " << Name[sort] << endl;
-
-                start = clock();
-                SortAl(b, n, sort);
-                end = clock();
-                double time_use = (double)(end - start) / CLOCKS_PER_SEC * 1000;
-                fout << "       " << time_use << endl;
-                cout << "       " << time_use << endl;
-                delete[] b;
-
-
-                long long int count = 0;
-                COUNT_SortAl(c, n, sort, count);
-                fout << "       " << count << endl;
-                cout << "       " << count << endl;
-                delete[] c;
+        if (argc == 5) {
+            if (_stricmp(argv[1], "-a") == 0) { // khác nhau giữa 1 và 3 là InputSize và Filename
+                ifstream fin(argv[3]);
+                //if (!fin)
+                //    command3();// Không tồn tại file => đây là size
+                //else
+                //    command1();
             }
-            delete[] a;
-            //fout << endl;
+            else
+                command4(argv[2], argv[3], argv[4]);
         }
-    }
+        else
+            if (argc == 6) {
+                if (_stricmp(argv[1], "-a") == 0);
+                  //  command2();
+                else
+                    command5(argv[2], argv[3], argv[4], argv[5]);
+            }
 
-    fout.close();
+
+
+
+
+
+
+
+
+    //int size[6] = { 10000, 30000, 50000, 100000, 300000, 500000 };
+    //vector <string> Name = { "SelectionSort", "Insertion_Sort", "Bubble_sort", "Shaker_Sort", "Shell_Sort", "Heap_Sort" , "Merge_Sort" , "Quick_Sort" , "Counting_Sort", "Radix_Sort", "Flash_Sort" };
+    //vector <string> Order = { "RandomData" , "SortedData", "ReverseData", "NearlySortedData" };
+    //clock_t start, end;   // Khai báo biến thời gian
+
+    //ofstream fout("output.txt");
+
+    //for (int OrderNo = 0; OrderNo < 4; OrderNo++) {
+    //    fout << "Data order:" << Order[OrderNo] << endl;
+    //    cout << "Kind of order: " << Order[OrderNo] << endl;
+    //    for (int id = 0; id < 6; id++) // size
+    //    {
+    //        int n = size[id];
+    //        int* a = new int[n];
+    //        GenerateData(a, size[id], OrderNo);
+    //        fout << "Data size" << n << endl;
+    //        cout << "   *size = " << n << endl;
+
+
+    //        for (int sort = 0; sort < 11; sort++)
+    //        {
+    //            int* b = new int[n];
+    //            int* c = new int[n];
+
+    //            for (int i = 0; i < size[id]; i++)
+    //                c[i] = b[i] = a[i];
+
+    //            fout << "       Kind of sort: " << Name[sort] << endl;
+    //            cout << "       Kind of sort: " << Name[sort] << endl;
+
+    //            start = clock();
+    //            SortAl(b, n, sort);
+    //            end = clock();
+    //            double time_use = (double)(end - start) / CLOCKS_PER_SEC * 1000;
+    //            fout << "       " << time_use << endl;
+    //            cout << "       " << time_use << endl;
+    //            delete[] b;
+
+
+    //            long long int count = 0;
+    //            COUNT_SortAl(c, n, sort, count);
+    //            fout << "       " << count << endl;
+    //            cout << "       " << count << endl;
+    //            delete[] c;
+    //        }
+    //        delete[] a;
+    //        //fout << endl;
+    //    }
+    //}
+
+    //fout.close();
 }
---
+++ cmd.h
@@ -0,0 +1,116 @@
+﻿#pragma once
+
+#include"GenerateData.h" // header chứa Generate Data
+#include"Timing.h" // header chứ các thuật toán sắp xếp _ đo thời gian
+#include "CountingComparision.h"// header chứa các thuật toán sắp xếp - đo số lần so sánh
+
+#include <string>
+
+
+string sort_names[] = {
+	"selection-sort", "insertion-sort","bubble-sort",
+	"shaker-sort",  "shell-sort",
+	"heap-sort", "merge-sort", "quick-sort", "couting-sort", "radix-sort", "flash-sort"
+};
+
+void command1();
+void command2();
+void command3();
+void command4(string Al1, string Al2, string InputFile)
+{
+	ifstream fin(InputFile);
+	if (!fin)
+	{
+		cout << "Cn not read file" << endl;
+		return;
+	}
+	int n; fin >> n;
+	int* a = new int[n], * b = new int[n], * c = new int[n], * d = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		fin >> a[i]; b[i] = a[i]; c[i] = a[i]; d[i] = a[i];
+	}
+	cout << "COMPARE MODE" << endl;
+	cout << "Algorithm: " << Al1 << " | " << Al2 << endl;
+	cout << "Inputfile: " << InputFile << endl;
+	cout << "InputSize: " << n << endl;
+	int x1 = 0, x2 = 0;// stand for type of Sort
+	for (int x1 = 0; x1 < 11; x1++)
+		if (sort_names[x1] == Al1)
+			break;
+	for (int x2 = 0; x2 < 11; x2++)
+		if (sort_names[x2] == Al1)
+			break;
+	clock_t start1, end1, start2, end2;
+	double time1 = 0, time2 = 0;
+	start1 = clock();
+	SortAl(a, n, x1);
+	end1 = clock();
+	start2 = clock();
+	SortAl(b, n, x2);
+	end2 = clock();
+	time1 = (double)end1 - (double)start1;
+	time2 = (double)end1 - (double)start1;
+	cout << "Runtime: " << time1 << " | " << time2 << endl;
+
+	// Comparison
+	long long com1 = 0, com2 = 0;
+	COUNT_SortAl(c, n, x1, com1);
+	COUNT_SortAl(d, n, x2, com2);
+	cout << "Comparisons: " << com1 << " | " << com2 << endl;
+}
+string Order[4] = { "-rand","-sorted","-rev","-nsorted" };
+string NameInputFile[] = { "input_1.txt","input_3.txt" ,"input_4.txt" ,"input_2.txt" };
+void command5(string Al1, string Al2, string InputSize, string InputOrder)
+{
+	cout << "COMPARE MODE" << endl;
+	cout << "Algorithm: " << Al1 << " | " << Al2 << endl;
+	cout << "Input size: " << InputSize << endl;
+	cout << "Input Order: " << InputOrder << endl;
+	int _order = 0;// input order;
+	for (_order = 0; _order < 4; _order++)
+		if (Order[_order] == InputOrder)
+			break;
+	int x1 = 0, x2 = 0;// stand for type of Sort
+	for (int x1 = 0; x1 < 11; x1++)
+		if (sort_names[x1] == Al1)
+			break;
+	for (int x2 = 0; x2 < 11; x2++)
+		if (sort_names[x2] == Al1)
+			break;
+
+	string InputFileName = NameInputFile[_order];
+	ofstream fout(InputFileName);
+	if (!fout)
+	{
+		cout << "Can not open input.txt" << endl;
+		return;
+	}
+	int n = stoi(InputSize);
+	fout << n << endl;
+	int* a = new int[n], * b = new int[n], * c = new int[n], * d = new int[n];
+	GenerateData(a, n, _order);
+	for (int i = 0; i < n; i++)
+	{
+		fout << a[i] << " ";
+		b[i] = a[i]; c[i] = a[i], d[i] = a[i];
+	}
+
+	clock_t start1, end1, start2, end2;
+	double time1 = 0, time2 = 0;
+	start1 = clock();
+	SortAl(a, n, x1);
+	end1 = clock();
+	start2 = clock();
+	SortAl(b, n, x2);
+	end2 = clock();
+	time1 = (double)end1 - (double)start1;
+	time2 = (double)end1 - (double)start1;
+	cout << "Runtime: " << time1 << " | " << time2 << endl;
+
+	// Comparison
+	long long int com1 = 0, com2 = 0;
+	COUNT_SortAl(c, n, x1, com1);
+	COUNT_SortAl(d, n, x2, com2);
+	cout << "Comparisons: " << com1 << " | " << com2 << endl;
+}
